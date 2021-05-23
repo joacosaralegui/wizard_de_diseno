a8d502d6d4347ad4162f6c7269ad52cff23d5689
Joaco Saralegui <joacosaralegui@gmail.com>
2021-05-22 21:42:13 -0300

    Refactoreo general de intents y utters. Actiones custom para cuando no entiende y fallback en wikipedia.

--- actions/actions.py
+++ actions/actions.py
@@ -11,17 +11,89 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import FollowupAction
 
+import wikipedia
+from wikipedia.wikipedia import summary
+wikipedia.set_lang("es")
+
+def get_latest_questions(events):
+    # Extrae los ultimos intents que tengan "pregunta_" en el nombre
+    return [e['parse_data']['response_selector']['default']['response']['intent_response_key'] for e in events if e['event'] == 'user' and 'pregunta_' in e['parse_data']['intent']['name']]
+
+class ActionClarification(Action):
+    def name(self) -> Text:
+        return "action_clarificacion"
+
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        utter = get_latest_questions(tracker.events)[0]
+        # pregunta_concepto/patron
+        # utter_pregunta_concepto/patron_clarificacion
+
+        # TODO: soportar cuando vuelve a no entender (1 nivel de recursividad de no entender)
+        # SI tiene clarificacion -> no me rompas las bolas
+        # TODO: chequear que exista y sino decir que -> nom me rompas als bolas
+
+        return [FollowupAction(name='utter_' + utter + '_clarificacion')]
+
+
+class ActionEjemplo(Action):
+    def name(self) -> Text:
+        return "action_ejempĺo"
+
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        utter = get_latest_questions(tracker.events)[0]
+        # pregunta_concepto/patron
+        # utter_pregunta_concepto/patron_clarificacion
+
+        # TODO: soportar cuando vuelve a no entender (1 nivel de recursividad de no entender)
+        # SI tiene clarificacion -> no me rompas las bolas
+        # TODO: chequear que exista y sino decir que -> nom me rompas als bolas
+
+        return [FollowupAction(name='utter_' + utter + '_ejemplo')]
+
+class ActionDefaultFallback(Action):
+    """Executes the fallback action and goes back to the previous state
+    of the dialogue"""
+
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+        ) -> List[Dict[Text, Any]]:
+    
+        text = tracker.latest_message['text']
+
+        try:
+            summary = wikipedia.summary(text, sentences=1)
+        except:
+            # TODO: add otras opciones!! (UTTER??)
+            summary = "Que pregunta más básica. No tenes algo mejor?"
+        dispatcher.utter_message(summary)
+        FollowupAction("action_listen")
+
+        # Revert user message which led to fallback.
+        return []
 
 class ActionExplainConcept(Action):
 
-     def name(self) -> Text:
-         return "action_explain_concept"
+    def name(self) -> Text:
+        return "action_explain_concept"
 
-     def run(self, dispatcher: CollectingDispatcher,
-             tracker: Tracker,
-             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        """
         last_entity_value = tracker.latest_message['entities'][0]['value']
         print(tracker.latest_message['entities'][0])
 
@@ -43,5 +115,5 @@ class ActionExplainConcept(Action):
             dispatcher.utter_message(response="utter_concepto_requerimiento-no-funcional")
         else:
             print('bueno no se que queres saber flaco')
-
+        """
         return []
