aeea263dc49d147813b037fbff0e5f5c6f883aa0
galopianciola <gpianciolabartol@alumnos.exa.unicen.edu.ar>
2021-05-25 12:06:53 -0300

    Agregando despacho de patrones mediante actions

--- actions/actions.py
+++ actions/actions.py
@@ -40,6 +40,102 @@ class ActionClarification(Action):
         return [FollowupAction(name='utter_' + utter + '_clarificacion')]
 
 
+class ActionDispatchPatronesArq(Action):
+    def name(self) -> Text:
+        return "action_dispatch_patrones_arq"
+
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        last_entity_value = tracker.latest_message['entities'][0]['value']
+        print(tracker.latest_message['entities'][0])
+
+        if last_entity_value == 'capas':
+            dispatcher.utter_message(response="utter_pregunta_patrones/capas")
+        elif last_entity_value == 'cliente-servidor':
+            dispatcher.utter_message(response="utter_pregunta_patrones/cliente-servidor")
+        elif last_entity_value == 'maestro-esclavo':
+            dispatcher.utter_message(response="utter_pregunta_patrones/maestro-esclavo")
+        elif last_entity_value == 'pipe-filter':
+            dispatcher.utter_message(response="utter_pregunta_patrones/pipe-filter")
+        elif last_entity_value == 'broker':
+            dispatcher.utter_message(response="utter_pregunta_patrones/broker")
+        elif last_entity_value == 'peer-to-peer':
+            dispatcher.utter_message(response="utter_pregunta_patrones/peer-to-peer")
+        elif last_entity_value == 'event-bus':
+            dispatcher.utter_message(response="utter_pregunta_patrones/event-bus")
+        elif last_entity_value == 'mvc':
+            dispatcher.utter_message(response="utter_pregunta_patrones/mvc")
+        elif last_entity_value == 'blackboard':
+            dispatcher.utter_message(response="utter_pregunta_patrones/blackboard")
+        elif last_entity_value == 'interpreter':
+            dispatcher.utter_message(response="utter_pregunta_patrones/interpreter")
+        else:
+            print('bueno no se que queres saber flaco')
+
+        return []
+
+class ActionDispatchPatronesPoo(Action):
+    def name(self) -> Text:
+        return "action_dispatch_patrones_poo"
+
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        last_entity_value = tracker.latest_message['entities'][0]['value']
+        print(tracker.latest_message['entities'][0])
+
+        if last_entity_value == 'abstract factory':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/abstract-factory")
+        elif last_entity_value == 'factory method':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/factory-method")
+        elif last_entity_value == 'builder':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/builder")
+        elif last_entity_value == 'prototype':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/prototype")
+        elif last_entity_value == 'singleton':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/singleton")
+        elif last_entity_value == 'adapter':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/adapter")
+        elif last_entity_value == 'bridge':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/bridge")
+        elif last_entity_value == 'composite':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/composite")
+        elif last_entity_value == 'decorator':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/decorator")
+        elif last_entity_value == 'facade':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/facade")
+        elif last_entity_value == 'flyweight':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/flyweight")
+        elif last_entity_value == 'proxy':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/proxy")
+        elif last_entity_value == 'chain of responsibility':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/chain-of-responsibility")
+        elif last_entity_value == 'command':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/command")
+        elif last_entity_value == 'memento':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/memento")
+        elif last_entity_value == 'iterator':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/iterator")
+        elif last_entity_value == 'mediator':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/mediator")
+        elif last_entity_value == 'observer':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/observer")
+        elif last_entity_value == 'state':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/state")
+        elif last_entity_value == 'strategy':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/strategy")
+        elif last_entity_value == 'template method':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/template-method")
+        elif last_entity_value == 'visitor':
+            dispatcher.utter_message(response="utter_pregunta_patrones_poo/visitor")
+        else:
+            print('bueno no se que queres saber flaco')
+
+        return []
+
 class ActionEjemplo(Action):
     def name(self) -> Text:
         return "action_ejempĺo"
